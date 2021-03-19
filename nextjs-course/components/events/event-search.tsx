9b5753782522cfd98c9fd91c3e4458c9dc38acfa
Denys Lins <denys.lins@hp.com>
2021-03-18 22:35:53 -0300

    Add form to filter events

--- nextjs-course/components/events/event-search.tsx
+++ nextjs-course/components/events/event-search.tsx
@@ -1,7 +1,40 @@
 import styles from "../../styles/event/event-search.module.css";
+import Button from "../ui/button";
 
 function EventSearch(props) {
-  return <h1 className={styles.list}>Teste</h1>;
+  return (
+    <form className={styles.form}>
+      <div className={styles.controls}>
+        <div className={styles.control}>
+          <label htmlFor="year">Year</label>
+          <select name="year" id="year">
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+            <option value="2023">2023</option>
+            <option value="2024">2024</option>
+          </select>
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="month">Year</label>
+          <select name="month" id="month">
+            <option value="1">January</option>
+            <option value="2">February</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">May</option>
+            <option value="6">June</option>
+            <option value="7">July</option>
+            <option value="8">August</option>
+            <option value="9">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
+          </select>
+        </div>
+        <Button>Find Events</Button>
+      </div>
+    </form>
+  );
 }
 
 export default EventSearch;
