c4cd466f159da66047cf22117cdb6e94dc11d9d7
Denys Lins <denys.lins@hp.com>
2021-03-31 21:14:23 -0300

    Add revalidate time

--- nextjs-course/pages/events/index.tsx
+++ nextjs-course/pages/events/index.tsx
@@ -1,15 +1,15 @@
-import { useRouter } from 'next/router'
-import EventList from '../../components/events/event-list'
-import EventSearch from '../../components/events/event-search'
-import { getAllEvents } from '../../data/dummy-data'
+import { useRouter } from "next/router";
+import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/event-search";
+import { getAllEvents } from "../../data/dummy-data";
 
-export default function EventsPage (props) {
-  const events = props.events
-  const router = useRouter()
+export default function EventsPage(props) {
+  const events = props.events;
+  const router = useRouter();
 
-  function findEventsHandler (year, month) {
-    const fullPath = `/events/${year}/${month}`
-    router.push(fullPath)
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
   }
 
   return (
@@ -18,17 +18,18 @@ export default function EventsPage (props) {
       <EventList events={events}></EventList>
       <div className="center">
         This page was generated at build time (Static Site Generation - SSG), in
-        the build machine. These three Events were collected from the database
-        in the build event.
+        the build machine, and is revalidated every 5 minutes. These three
+        Events were initially collected from the database in the build event.
       </div>
     </div>
-  )
+  );
 }
 
-export async function getStaticProps () {
+export async function getStaticProps() {
   return {
     props: {
-      events: getAllEvents()
-    }
-  }
+      events: getAllEvents(),
+    },
+    revalidate: 300,
+  };
 }
--- nextjs-course/pages/index.tsx
+++ nextjs-course/pages/index.tsx
@@ -1,26 +1,28 @@
-import { Fragment } from 'react'
-import EventList from '../components/events/event-list'
-import { getFeaturedEvents } from '../data/dummy-data'
+import { Fragment } from "react";
+import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../data/dummy-data";
 
-export default function Home (props) {
-  const featuredEvents = props.events
+export default function Home(props) {
+  const featuredEvents = props.events;
 
   return (
     <Fragment>
       <EventList events={featuredEvents} />
       <div className="center">
         This page was generated at build time (Static Site Generation - SSG), in
-        the build machine. These two Featured Events were collected from the
-        database in the build event.
+        the build machine, and is revalidated every 5 minutes. These two
+        Featured Events were initially collected from the database in the build
+        event.
       </div>
     </Fragment>
-  )
+  );
 }
 
-export async function getStaticProps () {
+export async function getStaticProps() {
   return {
     props: {
-      events: getFeaturedEvents()
-    }
-  }
+      events: getFeaturedEvents(),
+    },
+    revalidate: 300,
+  };
 }
